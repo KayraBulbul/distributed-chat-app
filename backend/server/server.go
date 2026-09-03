d3aa4a750edb193ec7e86a4936954eb5342b315e
Kayra <kayra19206@gmail.com>
2026-09-03 12:48:00 +1000

    feat: working connection from backend server and website client

--- backend/server/server.go
+++ backend/server/server.go
@@ -1,17 +1,18 @@
-package server
+package main
 
 import (
 	"flag"
-	"html/template"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/websocket"
 )
 
-var addr *string = flag.String("addr", "localhost:8080", "http service address")
-
-var upgrader websocket.Upgrader = websocket.Upgrader{}
+var upgrader websocket.Upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return r.Header.Get("Origin") == "http://localhost:5173"
+	},
+}
 
 type Hub struct {
 	clients    map[*Client]bool
@@ -90,13 +91,6 @@ func echo(h *Hub) http.Handler {
 	})
 }
 
-func home(w http.ResponseWriter, r *http.Request) {
-	err := homeTemplate.Execute(w, "ws://"+r.Host+"/echo")
-	if err != nil {
-		log.Println("template:", err)
-		return
-	}
-}
 
 func main() {
 	flag.Parse()
@@ -111,86 +105,5 @@ func main() {
 	go hub.run()
 
 	http.Handle("/echo", echo(&hub))
-	http.HandleFunc("/", home)
-	log.Fatal(http.ListenAndServe(*addr, nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
-var homeTemplate = template.Must(template.New("").Parse(`
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<script>  
-window.addEventListener("load", function(evt) {
-
-    var output = document.getElementById("output");
-    var input = document.getElementById("input");
-    var ws;
-
-    var print = function(message) {
-        var d = document.createElement("div");
-        d.textContent = message;
-        output.appendChild(d);
-        output.scroll(0, output.scrollHeight);
-    };
-
-    document.getElementById("open").onclick = function(evt) {
-        if (ws) {
-            return false;
-        }
-        ws = new WebSocket("{{.}}");
-        ws.onopen = function(evt) {
-            print("OPEN");
-        }
-        ws.onclose = function(evt) {
-            print("CLOSE");
-            ws = null;
-        }
-        ws.onmessage = function(evt) {
-            print("RESPONSE: " + evt.data);
-        }
-        ws.onerror = function(evt) {
-            print("ERROR: " + evt.data);
-        }
-        return false;
-    };
-
-    document.getElementById("send").onclick = function(evt) {
-        if (!ws) {
-            return false;
-        }
-        print("SEND: " + input.value);
-        ws.send(input.value);
-        return false;
-    };
-
-    document.getElementById("close").onclick = function(evt) {
-        if (!ws) {
-            return false;
-        }
-        ws.close();
-        return false;
-    };
-
-});
-</script>
-</head>
-<body>
-<table>
-<tr><td valign="top" width="50%">
-<p>Click "Open" to create a connection to the server, 
-"Send" to send a message to the server and "Close" to close the connection. 
-You can change the message and send multiple times.
-<p>
-<form>
-<button id="open">Open</button>
-<button id="close">Close</button>
-<p><input id="input" type="text" value="Hello world!">
-<button id="send">Send</button>
-</form>
-</td><td valign="top" width="50%">
-<div id="output" style="max-height: 70vh;overflow-y: scroll;"></div>
-</td></tr></table>
-</body>
-</html>
-`))
