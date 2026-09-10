6650f86cbfae154686625d2a026bcb4f807f74de
Kayra <kayra19206@gmail.com>
2026-09-10 17:34:19 +1000

    chore: put everything into one script and added server building

--- servers.sh
+++ servers.sh
@@ -7,6 +7,7 @@ start_or_run () {
         echo "Starting websocket servers..."
         docker start redis
         docker start caddy-balancer
+        docker start chat-app-db-1
         docker start ws1
         docker start ws2
         docker start ws3
@@ -23,9 +24,20 @@ case "$1" in
     docker stop ws2
     docker stop ws3
     docker stop caddy-balancer
+    docker stop chat-app-db-1
     docker stop redis
     ;;
+  build)
+    echo "Building websocket servers..."
+    docker run -d --name ws1 --hostname ws1 --env-file ./backend/config/.env --network ws-caddy ws-server:latest
+    docker run -d --name ws2 --hostname ws2 --env-file ./backend/config/.env --network ws-caddy ws-server:latest
+    docker run -d --name ws3 --hostname ws3 --env-file ./backend/config/.env --network ws-caddy ws-server:latest
+    ;;
+  remove)
+    echo "Removing websocket servers.."
+    docker rm ws1 ws2 ws3
+    ;;
   *)
-    echo "Usage: $0 {start|stop}"
+    echo "Usage: $0 {start|stop|build|remove}"
     exit 1
 esac
